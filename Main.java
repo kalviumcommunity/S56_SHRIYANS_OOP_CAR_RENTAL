fc99539ed3d0ed990ac8737ae8ba40d405b4cda4
shriyansjindal <shriyansjindal40517@gmail.com>
2024-11-04 12:07:30 +0530

    added abstract and virtual function

--- Main.java
+++ Main.java
@@ -1,12 +1,11 @@
 import java.util.*;
-public class Main{
-    public static void main(String[] args){
-    
+
+public class Main {
+    public static void main(String[] args) {
         ArrayList<Car> cars = new ArrayList<>();
         cars.add(new Car("1", "Model S", "Tesla", "Electric", 100));
         cars.add(new Car("2", "Civic", "Honda", "Sedan", 50));
         cars.add(new Car());
-
 
         cars.add(new ElectricCar("3", "Leaf", "Nissan", 80, 24)); // 24 hours battery life
 
@@ -17,6 +16,7 @@ public class Main{
 
         for (Car car : cars) {
             System.out.println(car.getDetails());
+            System.out.println("Rental cost for 5 days: " + car.calculateRentalCost(5)); // Calculate rental cost for 5 days
         }
         System.out.println("----------------------------");
         for (Customer customer : customers) {
@@ -25,7 +25,5 @@ public class Main{
 
         System.out.println("Total number of cars: " + Car.getCarCount());
         System.out.println("Total number of customers: " + Customer.getCustomerCount());
-        
-
     }
 }
